3df1bb1a31030b051469e50f9a0d83cb7c6c7af2
Sachin Sharma <sachin.sh1820@gmail.com>
2022-08-14 22:38:39 +0530

    some improvements

--- workers/sendAlert.js
+++ workers/sendAlert.js
@@ -1,13 +1,14 @@
 const CronJob = require("cron").CronJob;
 let Queue = require("bull");
 
-const {currentPrice} = require("../utilities/currentPrice");
+const { currentPrice } = require("../utilities/currentPrice");
+const { sendEmail } = require("../utilities/sendEmailNotification");
 
-let sendAlert = new CronJob("*/25 * * * * *", async function() {
+let sendAlert = new CronJob("*/25 * * * * *", async function () {
     let price = await currentPrice();
 
-    if(price.error)
+    if (price.error)
         return;
 
-    
+
 })
