2ffa6523ce8a80d316609fea7330f9ae88d2d528
Sachin Sharma <sachin.sh1820@gmail.com>
2022-08-15 11:52:18 +0530

    Remove Publisher Code

--- workers/sendAlert.js
+++ workers/sendAlert.js
@@ -1,14 +1,55 @@
+require('dotenv').config({ path: require("find-config")(".env") });
 const CronJob = require("cron").CronJob;
-let Queue = require("bull");
+const mongoose = require("mongoose");
+
+const Alert = require("../models/alert");
 
 const { currentPrice } = require("../utilities/currentPrice");
-const { sendEmail } = require("../utilities/sendEmailNotification");
+const { publish } = require("../utilities/publisher");
 
-let sendAlert = new CronJob("*/25 * * * * *", async function () {
-    let price = await currentPrice();
+//MongoDB Connect
+const dbUrl = "mongodb://localhost:27017/Krypto-App" || process.env.DB_URL;
+mongoose.connect(dbUrl);
 
-    if (price.error)
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error!"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
+
+const sendAlert = new CronJob("*/2 * * * * *", async function () {
+    let priceObj = await currentPrice();
+
+    if (priceObj.error)
         return;
 
+    let price = priceObj.data;
+    const alerts = await Alert.find({ status: "Created" });
 
-})
+    alerts.forEach((alert) => {
+        
+        if (alert.price == 240018) {
+
+            recipients = alert.email;
+            subject = `Bitcoint is UP!`;
+            message = `Price of Bitcoin has just exceeded your alert price of ${alert.price} USD. Current price is ${price} USD.`;
+            publish({recipients, subject, message});
+            alert.status = "Triggered"
+            alert.save();
+        }
+    })
+
+});
+
+sendAlert.start();
+
+/* const mailObj = {
+    from: "sachin.sh1820@gmail.com",
+    recipients: ["sachin.sh1800@gmail.com"],
+    subject: "Sending email by nodejs",
+    message: "Hello World;",
+} */
+
+//publish(mailObj);
+
+//module.exports.sendAlert = sendAlert;
